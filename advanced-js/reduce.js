518a015df6ed13d1002a4e4f942986074c5cd71c
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-04-05 19:13:51 +0000

    Initial Commit

---
+++ advanced-js/reduce.js
@@ -0,0 +1,37 @@
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 73, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, english: 88, maths: 97, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+const biggest = students.reduce((acc, curr) => {
+    let key = curr.results.english;
+    if (key > acc.max) {
+        acc.name = curr.name;
+        acc.max = key; 
+    }
+    return acc
+}, {name: "None", max: 0})
+
+console.log(biggest)
---
+++ jquery/challenge-2/index.js
@@ -0,0 +1,4 @@
+$(document).ready( () => {
+    $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even");
+});
